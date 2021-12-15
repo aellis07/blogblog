19887d63e48eeae19bdb470ff0d31826f075802f
Anthony Ellis <amellis245@gmail.com>
2021-12-15 09:19:36 -0500

    Revert "finished setting up server"
    This reverts commit 82c96b3f5a39f8b2538c57d50ed857094325e569.

--- models/Dashboard.js
+++ models/Dashboard.js
@@ -3,26 +3,4 @@ const sequelize = require("../config/connection");
 
 class Dashboard extends Model {}
 
-Dashboard.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    postTitle: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    postContent: { type: DataTypes.STRING },
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "Dashboard",
-  }
-);
-
-module.exports = Dashboard;
+Dashboard.init();
