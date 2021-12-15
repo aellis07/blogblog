5a9ec7561ae020d245823743ef50803499b5d864
Anthony Ellis <amellis245@gmail.com>
2021-12-15 12:37:01 -0500

    added delete route to userPost.js

--- controllers/api/userPost.js
+++ controllers/api/userPost.js
@@ -8,9 +8,33 @@ router.post("/", async (req, res) => {
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newProject, {
+      message: "Your message has been successfully posted!",
+    });
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const projectData = await Dashboard.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!projectData) {
+      res
+        .status(404)
+        .json({ message: "No blog found with this id! Please try again" });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
