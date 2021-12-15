c864d011fe8703ec03d527579f23837c8312aadf
Anthony Ellis <amellis245@gmail.com>
2021-12-15 09:22:33 -0500

    fcreated dashboard model

--- models/Dashboard.js
+++ models/Dashboard.js
@@ -3,4 +3,26 @@ const sequelize = require("../config/connection");
 
 class Dashboard extends Model {}
 
-Dashboard.init();
+Dashboard.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    postTitle: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    postContent: { type: DataTypes.STRING },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Dashboard",
+  }
+);
+
+module.exports = Dashboard;
